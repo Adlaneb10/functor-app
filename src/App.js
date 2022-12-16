14d821bb31919d2edeee15cdcb052620d1d65275
Adlaneb10 <c19367031@mytudublin.ie>
2022-12-16 14:53:03 +0000

    using functors to total nums in array

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@
 import './App.css';
 
 function App() {
+  let total = 0;
+  let arrayNums = [1,2,3,4,5,6,7];
+  arrayNums.forEach((num) => total += num);
+  console.log("Total " + total)
+
   return (
     <div className="App">
 
